1000b21a92f2c0dc44520fb8524f56df25ee6d4b
igor7x7 <https://github.com/igor7x7>
2022-03-23 14:00:35 -0300

    correção no bug do localstorage

--- add-palavra.js
+++ add-palavra.js
@@ -3,6 +3,8 @@ const button = document.getElementById('add-palavra')
 
 let nPalavra = ['gato', 'cachoro', 'cavalo', 'rinoceronte', 'camelo', 'tartaruga']
 
+localStorage.setItem('arraydepalavras', JSON.stringify(nPalavra))
+
 var addPalavra = function () {
     if (novaPalavra?.value == '') {
         console.log('erro')
